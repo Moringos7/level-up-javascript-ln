a299ff6ad2fb120dadd3f5260f1e2ae0b363093b
José Miguel <moringos7@gmail.com>
2023-10-20 21:36:35 +0000

    improve 4th with the help of the instructor

--- start/01_04_technical_books.js
+++ start/01_04_technical_books.js
@@ -36,7 +36,12 @@ class TechnicalBook extends Book {
     this.edition = edition
   }
 
-  get edition(){
-    return this.edition
+  getEdition(){
+    return `The current version of this book is ${this.edition}`;
   }
 }
+
+const CrackingTheCodingInterview = new TechnicalBook("Cracking The Coding Interview", "Gayle Laackmann McDowell", 1209123, 7, "2.3");
+
+console.log(CrackingTheCodingInterview.availability);
+console.log(CrackingTheCodingInterview.getEdition());
