52860e80c04512ffe474c4f1747a6fd05254f6fe
José Miguel <moringos7@gmail.com>
2023-10-23 15:58:49 +0000

    challenge 5 solved and remastered

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,28 @@
-// Write your code here
+class User{
+  #userName
+  #password
+  constructor(userName,password,age){
+    this.#userName = userName
+    this.#password = password
+    this.age = age
+  }
+}
+const user = new User("moringos","12342",24)
+
+console.log(user.username);
+console.log(user.password);
+console.log(user.age)
+
+//
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user2 = {
+  [username]: "moringos9",
+  [password]: "PatoPlano",
+  age: 24
+};
+
+console.log(user2.username);
+console.log(user2.password);
+console.log(user2.age);
