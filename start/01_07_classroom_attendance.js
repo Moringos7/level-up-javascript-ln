7f8dfe6fb155b7f0cfe43005dd5cd8d35e1b1f82
José Miguel <moringos7@gmail.com>
2023-11-14 15:52:37 +0000

    first deliver of 7th challenge

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,20 @@
 // Write your code here
+class ClassRoom{
+  constructor(hasTeachingAssistant, list){
+    this.hasTeachingAssistant = hasTeachingAssistant;
+    this.list = list;
+  }
+   
+}
+
+function getStudents(classRoom){
+  let studentList = classRoom.list.slice(1);
+  if(classRoom.hasTeachingAssistant){
+    studentList.shift();
+  }
+  return studentList;
+}
+const list = ["Oliver","Ren","Amy","Ahmed","Marcos"];
+const classRoom = new ClassRoom(false,list);
+console.log(getStudents(classRoom));
+console.log(classRoom.list);
