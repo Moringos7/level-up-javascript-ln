9fce5978cc4cc089a253130bb9eb812ccfa1322c
José Miguel <moringos7@gmail.com>
2023-11-28 14:46:57 +0000

    Challenge 9 finished

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,6 @@
-// Write your code here
+const numCoffee = [2,3,1,5];
+const coffeValue = 1.25;
+const total = numCoffee.reduce((acumulator, currentValue) =>
+  acumulator + (currentValue * coffeValue), 0
+);
+console.log(`The total bill is ${total}`);
